6d03f930497af4fa84835bd386e716b68c372c72
behzad.b.i.g <behzad.b.i.g@gmail.com>
2025-09-11 08:37:36 +0330

    cmt

--- WorkerService1/init-mongo.js
+++ WorkerService1/init-mongo.js
@@ -1,21 +1,23 @@
-﻿// init-mongo.js
-
-// Switch to admin database to create user
+﻿// Switch to admin database to create user
 db = db.getSiblingDB("admin");
 
-// Create user in admin database with access to scraper database
-db.createUser({
-  user: "myuser",
-  pwd: "mypassword",
-  roles: [
-    { role: "readWrite", db: "scraper" },
-    { role: "dbAdmin", db: "scraper" }
-  ],
-  mechanisms: ["SCRAM-SHA-1", "SCRAM-SHA-256"]
-});
+// Check if user already exists before creating
+if (!db.getUser("myuser")) {
+    db.createUser({
+        user: "myuser",
+        pwd: "mypassword",
+        roles: [
+            { role: "readWrite", db: "scraper" },
+            { role: "dbAdmin", db: "scraper" }
+        ],
+        mechanisms: ["SCRAM-SHA-1", "SCRAM-SHA-256"]
+    });
+}
 
 // Switch to scraper database
 db = db.getSiblingDB("scraper");
 
-// Create the Properties collection
-db.createCollection("Properties");
+// Create the Properties collection if it doesn't exist
+if (!db.getCollection("Properties")) {
+    db.createCollection("Properties");
+}
